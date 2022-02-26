50acff15b23bd04528ccc397168de3d6f7d0aa41
mijiFernandes <lkm366859@gmail.com>
2022-02-26 16:39:12 +0900

    First version of application moved into github

--- users/views.py
+++ users/views.py
@@ -83,7 +83,7 @@ class ChangePasswordView(SuccessMessageMixin, PasswordChangeView):
 def profile(request):
     if request.method == 'POST':
         user_form = UpdateUserForm(request.POST, instance=request.user)
-        profile_form = UpdateProfileForm(request.POST, request.FILES, instance=request.user)
+        profile_form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
 
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
@@ -92,6 +92,6 @@ def profile(request):
             return redirect(to='users-profile')
     else:
         user_form = UpdateUserForm(instance=request.user)
-        profile_form = UpdateProfileForm(instance=request.user)
+        profile_form = UpdateProfileForm(instance=request.user.profile)
 
     return render(request, 'users/profile.html', {'user_form': user_form, 'profile_form': profile_form})
