d398a9b51db24b10a58eabc32a3859e548a9e938
mijiFernandes <lkm366859@gmail.com>
2022-02-26 16:53:03 +0900

    First version of application moved into github

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,6 +15,13 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+POINTS_SETTINGS = {
+    "LOGIN": 5,
+    "LOGIN_STAY": 5,
+    "CREATE_ARTICLE": 5,
+    "ARTICLE_REPLY": 5,
+}
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
